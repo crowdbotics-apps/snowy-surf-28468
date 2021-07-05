0d8802be6af935d20b2632749062de699bb26b4a
Crowdbotics <team@crowdbotics.com>
2021-07-05 21:53:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/snowy_surf_28468/MainActivity.java
@@ -0,0 +1,15 @@
+package com.snowy_surf_28468;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "snowy_surf_28468";
+  }
+}
